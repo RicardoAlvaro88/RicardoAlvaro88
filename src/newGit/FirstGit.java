449bdbb7593cab1bb9b79c94853d9cd8d4c32de9
Krzysztof Wiechec <krzysztof.wiechec@ericsson.com>
2020-11-06 11:13:04 +0100

    tag 1.2

--- src/newGit/FirstGit.java
+++ src/newGit/FirstGit.java
@@ -5,7 +5,7 @@ public class FirstGit {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("Hello from git");
-System.out.println("Hello from git2");
+System.out.println("We are going to release tag with addnotation 1.2");
 	}
 
 }
